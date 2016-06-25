05ebc39bc12772d5770db1d5729e535290a4e268
Zack Coburn <zack@zackcoburn.com>
2016-06-24 23:33:11 -0500

    update

--- utility.js
+++ utility.js
@@ -829,6 +829,14 @@ Array.prototype.equals = function(b) {
   return true;
 }
 
+Math.sign = Math.sign || function(x) {
+  x = +x; // convert to a number
+  if (x === 0 || isNaN(x)) {
+    return x;
+  }
+  return x > 0 ? 1 : -1;
+}
+
 exports.decToHex = decToHex;
 exports.hexToDec = hexToDec;
 exports.roundToNearest = roundToNearest;
